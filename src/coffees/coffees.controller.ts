13e43d5796b7cdec5b3905ff43c38f42ba299585
Todd Ericskon <todd.erickson87@gmail.com>
2024-05-07 16:45:40 -0700

    Completion of Generating OpenAPI Specification

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -15,11 +15,14 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { Public } from 'src/common/decorators/public.decorator';
 import { Protocol } from 'src/common/decorators/protocol.decorator';
+import { ApiForbiddenResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('coffees')
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
   @Public()
   @Get()
   async findAll(
