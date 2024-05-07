17734df101fdb0b00aa13d9c700a2bf6f7658565
Todd Ericskon <todd.erickson87@gmail.com>
2024-05-07 12:15:42 -0700

    Completed Dependency Injection

---
+++ src/coffee-rating/coffee-rating.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { CoffeeRatingService } from './coffee-rating.service';
+import { CoffeesModule } from 'src/coffees/coffees.module';
+import { DatabaseModule } from 'src/database/database.module';
+
+@Module({
+  imports: [
+    DatabaseModule.register({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+    }),
+    CoffeesModule,
+  ],
+  providers: [CoffeeRatingService],
+})
+export class CoffeeRatingModule {}
--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,14 +1,51 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module, Scope } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from 'src/events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
+import { DataSource } from 'typeorm';
+
+// @Injectable()
+// export class CoffeeBrandsFactory {
+//   create() {
+//     /* ... do something ... */
+//     return ['buddy brew', 'nescafe'];
+//   }
+// }
+
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigServcie {}
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [
+    CoffeesService,
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentConfigService
+          : ProductionConfigServcie,
+    },
+    // { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] },
+    {
+      provide: COFFEE_BRANDS,
+      useFactory: async (dataSource: DataSource): Promise<string[]> => {
+        const coffeeBrands = await Promise.resolve(['buddy brew', 'nescafe']);
+        console.log('[!] Async factory');
+        return coffeeBrands;
+      },
+      scope: Scope.TRANSIENT,
+      // useFactory: (brandsFactory: CoffeeBrandsFactory) =>
+      //   brandsFactory.create(),
+      // inject: [CoffeeBrandsFactory],
+    },
+  ],
+  exports: [CoffeesService],
 })
 export class CoffeesModule {}
